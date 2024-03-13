8905b769c94b92ab1dd1c2fae287d81dcbde1edb
sangdo.park <parksangdonews@gmail.com>
2024-03-14 02:45:11 +0900

    Feat: Embeded Tomcat Init

--- src/main/java/com/ttp/mvcframework/MvcframeworkApplication.java
+++ src/main/java/com/ttp/mvcframework/MvcframeworkApplication.java
@@ -1,13 +1,30 @@
 package com.ttp.mvcframework;
 
-import org.springframework.boot.SpringApplication;
+import java.io.File;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class MvcframeworkApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MvcframeworkApplication.class, args);
+    public static void main(String[] args) throws LifecycleException {
+        String webappDirLocation = "webapps";
+
+        // start embedded tomcat server
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.addWebapp("", new File(webappDirLocation).getAbsolutePath()); // add webapp to tomcat
+
+        log.info("configuring app with basedir: {} ",
+            new File("./" + webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
+
     }
+
 
 }
