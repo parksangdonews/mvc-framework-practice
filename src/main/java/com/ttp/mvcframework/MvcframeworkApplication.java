da958fb5e084d560162db791a4296aa0a040d29a
sangdo.park <parksangdonews@gmail.com>
2024-03-14 00:43:01 +0900

    Feat: Project Init
    - Init & Set Git Ignore

---
+++ src/main/java/com/ttp/mvcframework/MvcframeworkApplication.java
@@ -0,0 +1,13 @@
+package com.ttp.mvcframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcframeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcframeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttp/mvcframework/MvcframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttp.mvcframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcframeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
