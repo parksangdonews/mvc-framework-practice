febaba8702c47293e9bc6e866da16fdf570b8a23
sangdo.park <parksangdonews@gmail.com>
2024-03-14 02:08:44 +0900

    Add: Add the setting for WEB and Lombok

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mvc-framework-practice
 MVC Framework 연습하면서 만들어보기.
+
+ENVIRONMENT
+- lombok
+- spring boot
--- src/main/java/com/ttp/mvcframework/MvcframeworkApplication.java
+++ src/main/java/com/ttp/mvcframework/MvcframeworkApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MvcframeworkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MvcframeworkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MvcframeworkApplication.class, args);
+    }
 
 }
